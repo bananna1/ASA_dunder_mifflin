9c1c3b0ae3ddd7a8c1405f4995a101b4434fa4b4
davipase <davipase@gmail.com>
2023-07-05 09:54:16 +0200

    cose

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-
-    host: "http://localhost:8080", // https://deliveroojs.onrender.com"
-
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA5ZmQ2NDllNzZlIiwibmFtZSI6Im1hcmNvIiwiaWF0IjoxNjc5OTk3Njg2fQ.6_zmgL_C_9QgoOX923ESvrv2i2_1bgL_cWjMw4M7ah4'
-
-}
-
-export default config;
